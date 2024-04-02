0fd19f6b86094dcb420ab36bd38bdde95aa3aa67
wongprom <>
2024-04-02 10:57:29 +0200

    Create turtle and move...
    Create Player class and method to move turtle on keypress up.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+# from car_manager import CarManager
+# from scoreboard import Scoreboard
 
 # Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing")
 screen.tracer(0)
+
 # Turtle
-t = Turtle()
-screen.exitonclick()
+t = Player()
+
+# Move turtle
+screen.listen()
+screen.onkey(t.go_up, "Up")
 
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
